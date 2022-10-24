9968d9d41bb9eab115ca385514106c9d37be425b
Justin Scott <hello@jrs.dev>
2022-10-23 19:58:38 -0700

    starting prototype 4

---
+++ prototype 4/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float Speed = 10;
+
+    private GameObject _focalPoint;
+    private Rigidbody2D _playerRb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _playerRb = GetComponent<Rigidbody2D>();
+        _focalPoint = GameObject.Find("Focal Point");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float verticalInput = Input.GetAxis("Vertical");
+        float horizontalInput = Input.GetAxis("Horizontal");
+
+        Vector2 direction = new Vector2(horizontalInput, verticalInput);
+        _playerRb.AddForce(direction * Speed);
+
+    }
+}
---
+++ prototype 4/Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    public GameObject EnemyPrefab;
+    public float XRange = 5.5f;
+    public float YRange = 2.5f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        float spawnPosX = Random.Range(-XRange, XRange);
+        float spawnPosY = Random.Range(-YRange, YRange);
+
+        Instantiate(EnemyPrefab, new Vector2(spawnPosX, spawnPosY), EnemyPrefab.transform.rotation);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
